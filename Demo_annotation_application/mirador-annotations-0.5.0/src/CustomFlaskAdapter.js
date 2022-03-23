abb46b69750ce2b3030123a16c4c610e89e54285
Ilya Dierckens <ilya.dierckens@student.howest.be>
2022-03-23 16:58:52 +0100

    Finished flask adapter

---
+++ Demo_annotation_application/mirador-annotations-0.5.0/src/CustomFlaskAdapter.js
@@ -0,0 +1,98 @@
+/** */
+export default class CustomFlaskAdapter {
+
+    constructor(annotationPageId, endpointUrl) {
+      this.annotationPageId = annotationPageId;
+      this.apiUrl = endpointUrl;
+    }
+  
+    /** */
+    async create(annotation) {
+      const emptyAnnoPage = {
+        id: this.annotationPageId,
+        items: [],
+        type: 'AnnotationPage',
+      };
+
+      const annotationPage = await this.all() || emptyAnnoPage;
+      annotationPage.items.push(annotation);
+
+       fetch(this.apiUrl, {
+            body: JSON.stringify(annotationPage),
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin':'*'
+            },
+            method: 'POST'
+        });
+        
+        return annotationPage;
+    }
+  
+    /** */
+    async update(annotation) {
+      const annotationPage = await this.all();
+      if (annotationPage) {
+        const currentIndex = annotationPage.items.findIndex((item) => item.id === annotation.id);
+        annotationPage.items.splice(currentIndex, 1, annotation);
+        await fetch(this.apiUrl, {
+            body: JSON.stringify(annotationPage),
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin':'*'
+            },
+            method: 'PATCH'
+        })
+        return annotationPage;
+      }
+      return null;
+    }
+  
+    /** */
+    async delete(annoId) {
+        const annotationPage = await this.all();
+        if (annotationPage) {
+            annotationPage.items = annotationPage.items.filter((item) => item.id !== annoId);
+        }
+        await fetch(this.apiUrl, {
+            body: JSON.stringify(annotationPage),
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin':'*'
+            },
+            method: 'DELETE'
+        })
+        return annotationPage;
+    }
+  
+    /** */
+    async get(annoId) {
+        const annotationPage = await this.all();
+        if (annotationPage) {
+            return annotationPage.items.find((item) => item.id === annoId);
+        }
+        return null;
+    }
+  
+    /** */
+    async all() { 
+        let annotationPageId = this.annotationPageId;
+        return await fetch(this.apiUrl, {
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin':'*'
+            },
+            method: 'GET'
+        })
+        .then(resp => resp.json())
+        .then(function(annotationPages){
+            let res = annotationPages[annotationPageId];
+            return res;
+        });
+    }
+  }
+  
