7911378f5810018f5c127553109f5e0ea9bfffe9
Ilya Dierckens <ilya.dierckens@student.howest.be>
2022-04-06 11:54:02 +0200

    Implemented test component that renders in the mirador client

--- Demo_annotation_application/client/mirador-annotations-0.5.0/demo/src/index.js
+++ Demo_annotation_application/client/mirador-annotations-0.5.0/demo/src/index.js
@@ -1,42 +1,51 @@
 import mirador from 'mirador/dist/es/src/index';
+import { func } from 'prop-types';
 import annotationPlugins from '../../src';
 import CustomFlaskAdapter from '../../src/CustomFlaskAdapter';
 import testComponent from './components/test-component';
 
-const endpointUrl = "http://127.0.0.1:5000/annotations";
-const config = {
-  annotation: {
-    adapter: (canvasId) => new CustomFlaskAdapter(canvasId, endpointUrl)
-  },
-  id: 'demo',
-  window: {
-    defaultSideBarPanel: 'annotations',
-    sideBarOpenByDefault: true,
-  },
-  //windows: [{
-    //loadedManifest: 'http://127.0.0.1:8887/1-test-painting-2/index.json',
-  //}],
-};
-const plugin = {
-  target: 'Workspace',
-  mode: 'wrap',
-  component: testComponent,
-  //connectOptions: additionalOptionsToPassToReduxConnect,
-  //mapStateToProps: mapStateToProps,
-  //mapDispatchToProps: mapDispatchToProps,
-  //reducers: {
-    //pluginState: pluginStateReducer,
-  //},
-  //saga: saga,
-  value: 'Button thing'
-};
+function initialiseMirador(){
+  const endpointUrl = "http://127.0.0.1:5000/annotations";
+  const config = {
+    annotation: {
+      adapter: (canvasId) => new CustomFlaskAdapter(canvasId, endpointUrl)
+    },
+    id: 'demo',
+    window: {
+      defaultSideBarPanel: 'annotations',
+      sideBarOpenByDefault: true,
+    },
+    //windows: [{
+      //loadedManifest: 'http://127.0.0.1:8887/1-test-painting-2/index.json',
+    //}],
+  };
+  const plugin = {
+    target: 'WorkspaceAdd',
+    mode: 'wrap',
+    component: testComponent,
+    //connectOptions: additionalOptionsToPassToReduxConnect,
+    //mapStateToProps: mapStateToProps,
+    //mapDispatchToProps: mapDispatchToProps,
+    //reducers: {
+      //pluginState: pluginStateReducer,
+    //},
+    //saga: saga,
+    value: 'Button thing'
+  };
 
-function getXmlDatabase(){
+  mirador.viewer(config, [...annotationPlugins, ...plugin]);
+}
+
+async function getXmlDatabase(){
   fetch('http://127.0.0.1:8887/database-dump/database-dump.xml')
   .then(response => response.text())
   .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
-  .then(data => console.log(data));
+  .then(function(data){
+    console.log(data)
+    console.log(data.getElementsByTagName("record")[0])
+  });
 }
 
 getXmlDatabase()
-mirador.viewer(config, [...annotationPlugins, ...plugin]);
+initialiseMirador()
+
